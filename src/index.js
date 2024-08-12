97ca5a2dded46fa6a60a3fc3408301294a1ab66b
badriyahs <badriyahalsalem@gmail.com>
2024-08-11 23:24:54 -0400

    Removed accidentally pushed folder

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { runtime } from '@observablehq/runtime';
-import define from '@observablehq/notebook';
-
-new runtime().module(define, name => {
-    if (name === "chart") return new Inspector(document.querySelector("#chart"));
-});
